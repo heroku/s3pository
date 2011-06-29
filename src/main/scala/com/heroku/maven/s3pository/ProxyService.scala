5da1c701386189c9b38b1f8664c4baa431c4f2c8
sclasen <sclasen@salesforce.com>
2011-06-29 12:13:23 -0700

    removing host conn limit and timeout

--- src/main/scala/com/heroku/maven/s3pository/ProxyService.scala
+++ src/main/scala/com/heroku/maven/s3pository/ProxyService.scala
@@ -61,8 +61,6 @@ class ProxyService(repositories: List[ProxiedRepository], groups: List[Repositor
       .sendBufferSize(1048576)
       .recvBufferSize(1048576)
       .hosts(new InetSocketAddress(host, port))
-      .hostConnectionLimit(Integer.MAX_VALUE)
-      .hostConnectionMaxIdleTime(5.seconds)
       .name(host)
       .logger(Logger.getLogger("finagle.client"))
     if(ssl)(builder = builder.tlsWithoutValidation())
