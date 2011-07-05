97f1f54a10f36b46cca3884047804156ec7653a8
sclasen <sclasen@salesforce.com>
2011-07-05 11:46:55 -0700

    add Stress test util

---
+++ src/test/scala/com/heroku/maven/s3pository/Stress.scala
@@ -0,0 +1,13 @@
+package com.heroku.maven.s3pository
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sclasen
+ * Date: 7/5/11
+ * Time: 10:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Stress {
+
+}
