c7101ee993c4db580973cc6f8046e3aec371aaec
Scott Clasen <scott@heroku.com>
2011-10-13 15:09:42 -0700

    updates for sbt based deploy

--- src/main/scala/com/heroku/maven/s3pository/LanguagePackUpdater.scala
+++ src/main/scala/com/heroku/maven/s3pository/LanguagePackUpdater.scala
@@ -4,7 +4,6 @@ import com.heroku.maven.s3pository.ProxyService._
 import com.heroku.maven.s3pository.S3rver._
 import com.twitter.finagle.stats.NullStatsReceiver
 import scala.io._
-import util.Properties
 import org.jboss.netty.handler.codec.http.HttpHeaders.Names._
 import org.jboss.netty.buffer.{ChannelBuffers, ChannelBuffer}
 
@@ -23,20 +22,51 @@ object LangPackSwitch {
 }
 
 object LangPackUpdate {
-  
+
   def readFile(fileName: String) = {
     ChannelBuffers.wrappedBuffer(Source.fromFile(fileName).mkString.getBytes)
   }
-  
+
   def putFile(bucket: String, fileName: String, content: ChannelBuffer) {
-      implicit val stats = NullStatsReceiver
-      val client = clientService(bucket + ".s3.amazonaws.com", 80, false, "s3 client for:" + bucket)
-      val s3put = put(fileName).headers(CONTENT_LENGTH -> content.readableBytes.toString, STORAGE_CLASS -> RRS,
-              CONTENT_TYPE -> "application/xml").s3headers(bucket)
-      s3put.setContent(content)
-      val response = client.service(s3put).get()
-      println(response.toString)
-      println(response.getContent.toString("UTF-8"))
-      client.release()    
+    implicit val stats = NullStatsReceiver
+    val client = clientService(bucket + ".s3.amazonaws.com", 80, false, "s3 client for:" + bucket)
+    val s3put = put(fileName).headers(CONTENT_LENGTH -> content.readableBytes.toString, STORAGE_CLASS -> RRS,
+      CONTENT_TYPE -> "application/xml").s3headers(bucket)
+    s3put.setContent(content)
+    val response = client.service(s3put).get()
+    println(response.toString)
+    println(response.getContent.toString("UTF-8"))
+    client.release()
   }
 }
+
+object Off {
+
+  def main(args: Array[String]) {
+    JavaOff.main(args)
+    PlayOff.main(args)
+    ScalaOff.main(args)
+  }
+
+}
+
+object On {
+
+  def main(args: Array[String]) {
+    JavaOn.main(args)
+    PlayOn.main(args)
+    ScalaOn.main(args)
+  }
+
+}
+
+
+object Update {
+
+  def main(args: Array[String]) {
+    JavaUpdate.main(args)
+    PlayUpdate.main(args)
+  }
+
+}
+
